c79de73cfc43727d160f7f6826d9ed3441d8e717
nelsonbolivar <nelson.bolivar@koombea.com>
2017-01-02 16:50:16 -0500

    Initial Commit

---
+++ KoombeaLoader/KoombeaLoader.h
@@ -0,0 +1,19 @@
+//
+//  KoombeaLoader.h
+//  KoombeaLoader
+//
+//  Created by Nelson Bolívar on 1/2/17.
+//  Copyright © 2017 Nelson Bolívar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KoombeaLoader.
+FOUNDATION_EXPORT double KoombeaLoaderVersionNumber;
+
+//! Project version string for KoombeaLoader.
+FOUNDATION_EXPORT const unsigned char KoombeaLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KoombeaLoader/PublicHeader.h>
+
+
